941af7e81039b84a7ea4a49b94718f2ce6b47060
bipro <biprocuet@gmail.com>
2023-10-10 16:56:22 +0600

    create test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js"; // Make sure the path is correct
+
 dotenv.config();
 
 mongoose
@@ -16,4 +18,10 @@ const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+app.use("/api/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
 });
