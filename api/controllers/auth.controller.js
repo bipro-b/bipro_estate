31c192b7a2c848b2967438cf96803169e267d3f1
bipro <biprocuet@gmail.com>
2023-10-13 12:08:24 +0600

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,9 +27,6 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email: email });
     if (!validUser) return next(errorHandler(404, "User not found"));
-
-
-    
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong Credential."));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
           <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4">
-          <Link to="/home">
+          <Link to="/">
             {" "}
             <li className="hidden sm:inline text-slate-700 hover:underline">
               Home
