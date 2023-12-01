23ce70fbc0502b06a615d969ccc9d52c35e3eb78
bipro <biprocuet@gmail.com>
2023-12-01 10:04:16 +0600

    change in listing

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -135,7 +135,7 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            {currentUser && listing.userRef !== currentUser._id && !contact && (
+            {!currentUser && (
               <button
                 onClick={() => setContact(true)}
                 className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
